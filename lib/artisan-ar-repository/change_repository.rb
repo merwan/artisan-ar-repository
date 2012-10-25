7e8b8bac238aa501b37915590c572ae44fbe09b3
Patrick Gombert <patrickgombert@gmail.com>
2012-10-24 22:38:22 -0500

    Adds find_range_by_project_id to ChangeRepository

--- lib/artisan-ar-repository/change_repository.rb
+++ lib/artisan-ar-repository/change_repository.rb
@@ -7,27 +7,27 @@ require 'artisan-ar-repository/models/project_change'
 module ArtisanArRepository
   class ChangeRepository
     include BaseRepository
-    
+
     def model_class
       ::Change
     end
-    
+
     def project_change_class
       ::ProjectChange
     end
-    
+
     def iteration_change_class
       ::IterationChange
     end
-    
+
     def story_change_class
       ::StoryChange
     end
-    
+
     def create_project_change(attributes = {})
       project_change_class.create!(attributes)
     end
-    
+
     def create_iteration_change(attributes = {})
       iteration_change_class.create!(attributes)
     end
@@ -35,11 +35,15 @@ module ArtisanArRepository
     def create_story_change(attributes = {})
       story_change_class.create!(attributes)
     end
-    
-    def find_all_by_project_id(id)
-      Change.find_all_by_project_id(id)
+
+    def find_all_by_project_id(project_id)
+      Change.find_all_by_project_id(project_id)
     end
-    
+
+    def find_range_by_project_id(project_id, starts_at, ends_at)
+      segment_length = ends_at - starts_at + 1
+      Change.where(:project_id => project_id).offset(starts_at).limit(segment_length)
+    end
   end
 end
 
